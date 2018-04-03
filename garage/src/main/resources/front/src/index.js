57dd3f4696cde5aec2e0ec152e3317b36ec712b4
pyxarez <pyxarez@gmail.com>
2018-04-03 23:05:54 +0300

    Fix unexisting dependency import

--- garage/src/main/resources/front/src/index.js
+++ garage/src/main/resources/front/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
@@ -8,11 +7,9 @@ import Main from './containers/Main';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-  <BrowserRouter>
-    <MuiThemeProvider>
-      <Main />
-    </MuiThemeProvider>
-  </BrowserRouter>
+  <MuiThemeProvider>
+    <Main />
+  </MuiThemeProvider>
 ), document.getElementById('root'))
 
 registerServiceWorker();
