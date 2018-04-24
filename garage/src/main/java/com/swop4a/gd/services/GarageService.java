f90a91ec07ce5e0001665a71dac3a82d1f3c0e60
Alexandr Pavkin <alexandr300896@gmail.com>
2018-04-24 21:47:50 +0400

    Add description in char pom.xml; Add javadocs

--- garage/src/main/java/com/swop4a/gd/services/GarageService.java
+++ garage/src/main/java/com/swop4a/gd/services/GarageService.java
@@ -6,11 +6,23 @@ import java.util.List;
 
 public interface GarageService {
 
+	/**
+	 * Find all garages.
+	 */
 	List<Garage> findAll();
 
+	/**
+	 * Find garage by garageId.
+	 */
 	Garage findOne(Long garageId);
 
+	/**
+	 * Add car to garage using garageId.
+	 */
 	void addCar(Car car, Long garageId);
 
+	/**
+	 * Remove car by id.
+	 */
 	void removeCar(Long carId);
 }
